00a398cf361ba8719fb5f7ad31da06ad92649b08
Andrey <anridegod@yandex.ru>
2016-02-28 20:29:36 +0600

    extracted client lib to separate project

--- VCT.Test/Program.cs
+++ VCT.Test/Program.cs
@@ -1,210 +1,27 @@
 ﻿using System;
 using System.IO;
-using System.Linq;
-using System.Net;
-using System.Net.Http;
-using System.Net.Http.Headers;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Remote;
 
 
 namespace VCT.Test
 {
-	class Program
+	internal class Program
 	{
-		static string baseAddress = "http://localhost:9111/";
-		static string url = string.Format("{0}/tests/{1}/stable", baseAddress, "BBBD");
-		static FileInfo file = new FileInfo(@"F:\Projects\VCT.Test\T.txt");
-		static FileInfo file2 = new FileInfo(@"F:\Projects\VCT.Test\Bender.jpeg");
-		
-		static void Main(string[] args)
+		private static void Main(string[] args)
 		{
 			RemoteWebDriver driver = new ChromeDriver();
 			FileInfo outputScreen = new FileInfo(@"C:\projects\VCT\Output\TESTOUTPUT.png");
-			var core = Core.Instance;
+			var core = TestCore.Instance;
 
 			driver.Manage().Window.Maximize();
 			driver.Navigate().GoToUrl("https://vk.com");
 //			driver.Navigate().GoToUrl("http://www.google.com/");
-			core.MakeScreenshot(driver, outputScreen, "FAKETEST");
+			var equal = core.IsPageScreensAreEqual(driver, outputScreen, "FAKETEST22");
+			Console.WriteLine(equal);
 			driver.Quit();
 
 			Console.ReadLine();
-		}
-
-		public async static void DoSimpleTest()
-		{
-			using (var httpClient = new HttpClient())
-			{
-				var result = httpClient.GetAsync(url).Result;
-				if (result.StatusCode == HttpStatusCode.NoContent)
-				{
-					Console.WriteLine("NO CONTENT");
-					return;
-				}
-				var stream = await result.Content.ReadAsStreamAsync();
-				using (var file = File.Open(@"F:\Projects\VCT.Test\Output\TTTT.txt", FileMode.CreateNew))
-				{
-					await stream.CopyToAsync(file);
-				}
-				Console.WriteLine(result);
-			}
-		}
-
-		//stable
-		public async static void GetData()
-		{
-			using (var httpClient = new HttpClient())
-			{
-				var result = httpClient.GetAsync(url).Result;
-				var stream = await result.Content.ReadAsStreamAsync();
-				using (var file = File.Open(@"F:\Projects\VCT.Test\Output\TTTT.txt", FileMode.CreateNew))
-				{
-					await stream.CopyToAsync(file);
-				}
-				Console.WriteLine(result);
-			}
-		}
-
-		public async static void GetMultiData()
-		{
-			using (var httpClient = new HttpClient())
-			{
-				var result = httpClient.GetAsync(url).Result;
-				var stream = await result.Content.ReadAsMultipartAsync();
-				foreach (var content in stream.Contents)
-				{
-					var fName = content.Headers.GetValues("fileName").First();
-					using (var fileStream = File.Open(Path.Combine(@"F:\Projects\VCT.Test\Output", fName), FileMode.Create))
-					{
-						await content.CopyToAsync(fileStream);
-					}
-				}
-				Console.WriteLine(result);
-			}
-		}
-
-
-
-		public static void UploadData()
-		{
-			using (var httpClient = new HttpClient())
-			{
-				var fileStream = File.Open(file.FullName, FileMode.Open);
-				StreamContent content = new StreamContent(fileStream);
-				content.Headers.Add("fileName", file.Name);
-
-				var result = httpClient.PostAsync(url, content).Result;
-				Console.WriteLine(result);
-			}
-		}
-
-		private struct ImageData
-		{
-			public Stream ImageStream { get; set; }
-			public string ImageName { get; set; }
-		}
-
-		/// <summary>
-		/// THIS WORKS PERFECTLY!!!
-		/// </summary>
-		public static void UpData()
-		{
-			using (var httpClient = new HttpClient())
-			{
-				var fileStream = File.Open(file.FullName, FileMode.Open);
-				var fileStream2 = File.Open(file2.FullName, FileMode.Open);
-				var content = new MultipartFormDataContent
-				{
-					{new StreamContent(fileStream), "\"file\"", string.Format("\"{0}\"", file.Name)},
-					{new StreamContent(fileStream2), "\"file\"", string.Format("\"{0}\"", file2.Name)}
-				};
-
-				var result = httpClient.PostAsync(url, content).Result;
-				Console.WriteLine(result);
-			}
-		}
-
-		public static void HttpUploadFile(string url, string file, string paramName, string contentType)
-		{
-
-			string boundary = "---------------------------" + DateTime.Now.Ticks.ToString("x");
-			byte[] boundarybytes = System.Text.Encoding.ASCII.GetBytes("\r\n--" + boundary + "\r\n");
-
-			HttpWebRequest wr = (HttpWebRequest)WebRequest.Create(url);
-			wr.ContentType = "multipart/form-data; boundary=" + boundary;
-			wr.Method = "POST";
-			wr.KeepAlive = true;
-			wr.Credentials = CredentialCache.DefaultCredentials;
-
-			Stream rs = wr.GetRequestStream();
-
-			rs.Write(boundarybytes, 0, boundarybytes.Length);
-
-			string headerTemplate = "Content-Disposition: form-data; name=\"{0}\"; filename=\"{1}\"\r\nContent-Type: {2}\r\n\r\n";
-			string header = string.Format(headerTemplate, paramName, file, contentType);
-			byte[] headerbytes = System.Text.Encoding.UTF8.GetBytes(header);
-			rs.Write(headerbytes, 0, headerbytes.Length);
-
-			FileStream fileStream = new FileStream(file, FileMode.Open, FileAccess.Read);
-			byte[] buffer = new byte[4096];
-			int bytesRead = 0;
-			while ((bytesRead = fileStream.Read(buffer, 0, buffer.Length)) != 0)
-			{
-				rs.Write(buffer, 0, bytesRead);
-			}
-			fileStream.Close();
-
-			byte[] trailer = System.Text.Encoding.ASCII.GetBytes("\r\n--" + boundary + "--\r\n");
-			rs.Write(trailer, 0, trailer.Length);
-			rs.Close();
-
-			WebResponse wresp = null;
-			try
-			{
-				wresp = wr.GetResponse();
-				Stream stream2 = wresp.GetResponseStream();
-				StreamReader reader2 = new StreamReader(stream2);
-				Console.WriteLine("File uploaded, server response is: {0}", reader2.ReadToEnd());
-			}
-			catch (Exception ex)
-			{
-				Console.WriteLine("Error uploading file", ex);
-				if (wresp != null)
-				{
-					wresp.Close();
-					wresp = null;
-				}
-			}
-			finally
-			{
-				wr = null;
-			}
-		}
-	}
-
-	public class FileUploadResult
-	{
-		public string LocalFilePath { get; set; }
-		public string FileName { get; set; }
-		public long FileLength { get; set; }
-	}
-
-	public class UploadMultipartFormProvider : MultipartFormDataStreamProvider
-	{
-		public UploadMultipartFormProvider(string rootPath) : base(rootPath) { }
-
-		public override string GetLocalFileName(HttpContentHeaders headers)
-		{
-			if (headers != null &&
-				headers.ContentDisposition != null)
-			{
-				return headers
-					.ContentDisposition
-					.FileName.TrimEnd('"').TrimStart('"');
-			}
-
-			return base.GetLocalFileName(headers);
 		}
 	}
 }
---
+++ VCT.Test/TestCore.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using OpenQA.Selenium.Remote;
+using VCT.Client;
+
+namespace VCT.Test
+{
+	public sealed class TestCore
+	{
+		private static readonly TestCore instance = new TestCore();
+
+		private TestCore()
+		{
+			//necessary to identify time when all tests are finished;
+			AppDomain.CurrentDomain.DomainUnload += CurrentDomainOnDomainUnload;
+		}
+
+		public bool IsPageScreensAreEqual(RemoteWebDriver driver, FileInfo outputScreenFile, string testName)
+		{
+			var screenshotMaker = new ScreenshotMaker(driver);
+			screenshotMaker.MakeFullScreenshot(outputScreenFile);
+			var stableDirectory = new DirectoryInfo(Path.Combine(outputScreenFile.Directory.FullName, "STABLE"));
+			var diffDirectory = new DirectoryInfo(Path.Combine(outputScreenFile.Directory.FullName, "DIFF"));
+
+			var vctCore = new Core();
+			var success = vctCore.GetStableFiles(stableDirectory, testName);
+			if (!success)
+			{
+				vctCore.SaveTestingFiles(outputScreenFile.Directory, testName);
+				return false;
+			}
+
+			var equal = CompareScreenshots(outputScreenFile, stableDirectory, diffDirectory);
+			if (!equal)
+			{
+				vctCore.SaveTestingFiles(outputScreenFile.Directory, testName);
+				vctCore.SaveDiffFiles(diffDirectory, testName);
+			}
+			return equal;
+		}
+
+		private bool CompareScreenshots(FileInfo outputTestingFile, DirectoryInfo stableFilesFolder, DirectoryInfo diffDirectory)
+		{
+			var stableImageFile = new FileInfo(Path.Combine(stableFilesFolder.FullName, outputTestingFile.Name));
+
+			return ImageFileComparer.ComparingFilesAreEqual(stableImageFile, outputTestingFile, diffDirectory);
+		}
+
+		private void CurrentDomainOnDomainUnload(object sender, EventArgs eventArgs)
+		{
+			//Pass end time of group test to server
+			MessageBox.Show("All is done");
+		}
+
+		public static TestCore Instance
+		{
+			get { return instance; }
+		}
+	}
+}
