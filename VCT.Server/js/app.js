93755a1a260b2cad87434fc0afdbed15e20e55e0
Andrey <anridegod@yandex.ru>
2016-04-13 15:35:12 +0600

    changed main indx page. Show history using res api method and navigate to preview results on click. Issue #9

--- VCT.Server/js/app.js
+++ VCT.Server/js/app.js
@@ -1,33 +1,71 @@
 var fakeData = [{
-   "TestName" : "",
-   "Artifacts" : [
-    {
-       "StableFile" : {
-        },
-       "TestingFile" : {
-        },
-       "DiffFile" : {
-        }
-   }] 
+    "TestName": "",
+    "Artifacts": [
+        {
+            "StableFile": {
+                "Name" : "",
+                "Path" : "",
+                "RelativePath" : ""
+            },
+            "TestingFile": {
+                "Name" : "",
+                "Path" : "",
+                "RelativePath" : ""
+            },
+            "DiffFile": {
+                "Name" : "",
+                "Path" : "",
+                "RelativePath" : ""
+            }
+        }]
+}]
+
+var fakeHistoryData = [{
+    "DateStarted": "",
+	"DateCompleted": "",
+	"Passed": 0,
+	"Failed": 0,
+	"Id": 0,
+	"Tests": [{
+		"TestName": "",
+    "Artifacts": [
+        {
+            "StableFile": {
+                "Name" : "",
+                "Path" : "",
+                "RelativePath" : ""
+            },
+            "TestingFile": {
+                "Name" : "",
+                "Path" : "",
+                "RelativePath" : ""
+            },
+            "DiffFile": {
+                "Name" : "",
+                "Path" : "",
+                "RelativePath" : ""
+            }
+        }]
+	}]
 }]
 
 var TestNameElement = React.createClass({
-    render: function(){
+    render: function() {
         return (
             <div className="navbar-header">
-                  <a className="navbar-brand">{this.props.testName}</a>
+                <a className="navbar-brand">{this.props.testName}</a>
             </div>
         );
     }
 })
 
 var NavigationResultBar = React.createClass({
-    render: function(){
+    render: function() {
         const diffIconClass = this.props.showDiff ? "fa fa-eye-slash" : "fa fa-eye";
         const imageIndex = this.props.currentImageIndex + 1;
-        const display = this.props.hasDiff ? {display: "inline"} : {display: "none"};
-        
-        return(
+        const display = this.props.hasDiff ? { display: "inline" } : { display: "none" };
+
+        return (
             <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
                 <ul className="nav navbar-nav">
                     <li><a href="#" id="previousTest" onClick={this.props.clickEvent}><i  id="previousTest" className="fa fa-step-backward"></i></a></li>
@@ -48,13 +86,13 @@ var NavigationResultBar = React.createClass({
 });
 
 var TestResultContainer = React.createClass({
-    render: function(){
+    render: function() {
         return (
             <div className="flexChild rowParent">
-                <div id="leftImage" className="flexChild">
+                <div id="leftImage" onScroll={this.props.scrollEvent} className="flexChild">
                     <img src={this.props.left}/>
                 </div>
-                <div id="rightImage" className="flexChild">
+                <div id="rightImage" onScroll={this.props.scrollEvent} className="flexChild">
                     <img src={this.props.right} />
                 </div>
             </div>
@@ -62,66 +100,50 @@ var TestResultContainer = React.createClass({
     }
 });
 
-var PageContent = React.createClass({
-    loadDataFromServer: function() {
-        $.ajax({
-            url: this.props.url,
-            dataType: 'json',
-            cache: false,
-            success: function(data) {
-                var maxTests = data.length;
-                var hasDiff = data[0].Artifacts[0].DiffFile.Name !== undefined;
-                this.setState({
-                    imageIndex: 0,
-                    showDiff: false,
-                    hasDiff: hasDiff,
-                    testData: data,
-                    maxTests: maxTests,
-                    maxImages: data[0].Artifacts.length,
-                    imageName: this.getCurrentImageName(data[0].Artifacts[0]) 
-            });
-            }.bind(this),
-            error: function(xhr, status, err) {
-                console.error(this.props.url, status, err.toString());
-            }.bind(this)
-        });
-    },
-    getCurrentImageName: function(artifact){
-        if(artifact.TestingFile.Name !== undefined){
+var ResultsPreviewCotent = React.createClass({
+    getCurrentImageName: function(artifact) {
+        if (artifact.TestingFile.Name !== "") {
             return artifact.TestingFile.Name;
         }
-        if(artifact.StableFile.Name !== undefined){
+        if (artifact.StableFile.Name !== "") {
             return artifact.StableFile.Name;
         }
-        if(artifact.DiffFile.Name !== undefined){
+        if (artifact.DiffFile.Name !== "") {
             return artifact.DiffFile.Name;
         }
         return "";
     },
-    componentDidMount: function(){
+    componentDidMount: function() {
         $(document.body).on('keydown', this.handleKeyDown);
-        this.loadDataFromServer(); 
+        // this.loadDataFromServer();
     },
-    componenWillUnmount: function(){
-        $(document.body).off('keydown', this.handleKeyDown);  
+    componenWillUnmount: function() {
+        $(document.body).off('keydown', this.handleKeyDown);
     },
-    getInitialState: function(){
+    getInitialState: function() {
+        var artifacts = this.props.data[0].Artifacts;
+        
+        var imageName = this.getCurrentImageName(artifacts[0]);
+        var maxImages = artifacts.length;
+        var maxTests = this.props.data.length;
+        var hasDiff = artifacts[0].DiffFile.Name !== "";
+        
         return ({
             //index of the test in current fails
             testIndex: 0,
             //image index in the current test
             imageIndex: 0,
             //max images count
-            maxImages: 0,
-            maxTests: 0,
-            imageName: "",
+            maxImages: maxImages,
+            maxTests: maxTests,
+            imageName: imageName,
             //show diff image instead of stable or not
             showDiff: false,
-            hasDiff: false,
-            testData: fakeData
+            hasDiff: hasDiff,
+            testData: this.props.data
         });
     },
-    handleChildClick: function(event){
+    handleChildClick: function(event) {
         var imageIndex = this.state.imageIndex;
         var showDiff = this.state.showDiff;
         var hasDiff = this.state.hasDiff;
@@ -129,128 +151,144 @@ var PageContent = React.createClass({
         var currentImageName = this.state.imageName;
         var maxImages = this.state.maxImages;
         var id;
-        
-        if(typeof event === "string"){
+
+        if (typeof event === "string") {
             id = event;
-        }else{
+        } else {
             id = event.target.id;
         }
-                
-        if(id === "showDiff" && hasDiff){
+
+        if (id === "showDiff" && hasDiff) {
             showDiff = !this.state.showDiff;
         }
-        if(id === "nextFail"){
-            if(this.state.imageIndex === (this.state.maxImages - 1)) return;
+        if (id === "nextFail") {
+            if (this.state.imageIndex === (this.state.maxImages - 1)) return;
             
             imageIndex = this.state.imageIndex + 1;
             showDiff = false;
         }
-        if(id === "previousFail"){
-            if(this.state.imageIndex === 0) return;
+        if (id === "previousFail") {
+            if (this.state.imageIndex === 0) return;
             imageIndex = this.state.imageIndex - 1,
-            showDiff = false
+                showDiff = false
         }
-        if(id === "previousTest"){
-            if(this.state.testIndex === 0) return;
-            
+        if (id === "previousTest") {
+            if (this.state.testIndex === 0) return;
+
             imageIndex = 0;
             showDiff = false;
             testIndex = this.state.testIndex - 1;
-            
+
             maxImages = this.state.testData[testIndex].Artifacts.length;
         }
-         if(id === "nextTest"){
-            if(this.state.testIndex === (this.state.maxTests - 1)) return;
-            
+        if (id === "nextTest") {
+            if (this.state.testIndex === (this.state.maxTests - 1)) return;
+
             testIndex = this.state.testIndex + 1;
             imageIndex = 0;
             showDiff = false;
-            
+
             maxImages = this.state.testData[testIndex].Artifacts.length;
         }
-        if(id === "acceptFail"){
-            var url = 'tests//' + this.state.testData[this.state.testIndex].TestName + '//stable';
+        if (id === "acceptFail") {
+            var testName = this.state.testData[this.state.testIndex].TestName;
+            if(testName === "") return;
+            
+            var url = 'tests//' + testName + '//stable';
             $.ajax({
                 url: url,
                 dataType: 'text',
                 type: 'POST',
                 cache: false,
-                success: function(data){
-                    this.loadDataFromServer();
+                success: function(data) {
+                    console.log(data);
                 }.bind(this),
-                error: function(xhr, status, err){
+                error: function(xhr, status, err) {
                     console.error(url, status.err.toString());
                 }.bind(this)
             });
             return;
         }
-        
+
         currentImageName = this.getCurrentImageName(this.state.testData[testIndex].Artifacts[imageIndex]);
-        hasDiff = this.state.testData[testIndex].Artifacts[imageIndex].DiffFile.Name !== undefined;
-        
+        hasDiff = this.state.testData[testIndex].Artifacts[imageIndex].DiffFile.Name !== "";
+
         this.setState({
             imageIndex: imageIndex,
             showDiff: showDiff,
             hasDiff: hasDiff,
-            testIndex : testIndex,
+            testIndex: testIndex,
             imageName: currentImageName,
             maxImages: maxImages
         })
     },
-    handleKeyDown: function(event){
+    handleScroll: function(e){
+        var current = e.target;
+        var $other = current.id === "leftImage" ? $("#rightImage") : $("#leftImage");
+        $other.off('scroll');
+        var other = $other[0];
+        
+        var verticalPercentage = current.scrollTop / (current.scrollHeight - current.offsetHeight);
+        var horizontalPercentage = current.scrollLeft / (current.scrollWidth - current.offsetWidth);
+        other.scrollLeft = horizontalPercentage * (other.scrollWidth - other.offsetWidth);;
+        other.scrollTop = verticalPercentage * (other.scrollHeight - other.offsetHeight);
+        
+        setTimeout(function() { $other.on('scroll', this.handleScroll); }, 10);
+    },
+    handleKeyDown: function(event) {
         //rigth arrow
-        if(event.keyCode === 39){
+        if (event.keyCode === 39) {
             this.handleChildClick("nextFail");
             return;
         }
         //left arrow
-        if(event.keyCode === 37){
+        if (event.keyCode === 37) {
             this.handleChildClick("previousFail");
             return;
         }
         //up arrow
-        if(event.keyCode === 38){
+        if (event.keyCode === 38) {
             this.handleChildClick("previousTest");
             return;
         }
         //down arrow
-        if(event.keyCode === 40){
+        if (event.keyCode === 40) {
             this.handleChildClick("nextTest");
             return;
         }
         //space
-        if(event.keyCode === 32 && this.state.hasDiff){
+        if (event.keyCode === 32) {
             this.handleChildClick("showDiff");
             return;
         }
     },
-    render: function(){
+    render: function() {
         const {TestName: testName, Artifacts: artifacts} = this.state.testData[this.state.testIndex];
+        
         const testingImage = this.state.showDiff ? artifacts[this.state.imageIndex].DiffFile : artifacts[this.state.imageIndex].TestingFile;
-        
         const imageName = this.state.imageName;
-        
+
         var testingImagePath;
-        
-        if(testingImage.RelativePath !== undefined){
+
+        if (testingImage.RelativePath !== "") {
             testingImagePath = testingImage.RelativePath;
-        } else{
-            if(this.state.showDiff){
+        } else {
+            if (this.state.showDiff) {
                 testingImagePath = "images\\nodiff.png";
-            } else{
+            } else {
                 testingImagePath = "images\\notesting.png"
             }
         }
-                
+
         const stableImage = artifacts[this.state.imageIndex].StableFile;
-        var stableImagePath 
-        if(stableImage.RelativePath !== undefined){
-            stableImagePath = stableImage.RelativePath; 
-        }else{
-            stableImagePath = "images\\nostable.png"; 
+        var stableImagePath
+        if (stableImage.RelativePath !== "") {
+            stableImagePath = stableImage.RelativePath;
+        } else {
+            stableImagePath = "images\\nostable.png";
         }
-        
-        return(
+
+        return (
             <div className="flexChild columnParent"> 
                 <nav className="navbar navbar-default">
                     <div className="container-fluid">
@@ -264,28 +302,137 @@ var PageContent = React.createClass({
                         />
                     </div>
                 </nav>
-                <TestResultContainer left={stableImagePath} right={testingImagePath}/>
+                <TestResultContainer left={stableImagePath} right={testingImagePath} scrollEvent={this.handleScroll}/>
             </div>
         );
     }
 });
 
+var HistoryBar = React.createClass({
+    render: function() {
+        return (
+            <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                <ul className="nav navbar-nav">
+                    <li><p className="navbar-text"><i className="ion-clock"></i></p></li>
+                    <li><p className="navbar-text">Test Results</p></li>
+                </ul>
+            </div>
+        )
+    }
+});
+
+var HistoryItem = React.createClass({
+    render: function() {
+        const awesomeStyle = {
+            height: '80px',
+            marginTop: '15px',  
+        };
+        const evenMoreAwesomeStyle = {
+            display: 'block',
+            width: '100%',
+            height: '100%',
+            color: 'rgba(0,0,0,0.87)',
+        };
+
+        return (
+            <div className="col-xs-12 col-md-6" style={awesomeStyle} onClick={this.props.clickEventHandler}>
+                <a href="#" className="row run" style={evenMoreAwesomeStyle}>
+                    <div className="col-xs-6 info">
+                        <p className="title">Some title with ID: {this.props.id}</p>
+                        <p className="details">Started: {this.props.dateStarted}. Completed: {this.props.dateCompleted}</p>
+                    </div>
+                    <div className="col-xs-3 stat passed">
+                        <p className="title">Passed</p>
+                        <p className="amount">{this.props.passed}</p>
+                    </div>
+                    <div className="col-xs-3 stat failed">
+                        <p className="title">Failed</p>
+                        <p className="amount">{this.props.failed}</p>
+                    </div>
+                    
+                </a>
+            </div>
+        )
+    }
+});
+
+var HistoryContent = React.createClass({
+    render: function(){
+        var clickHanlded = this.props.clickEventHandler;
+        
+        return (
+            <div>
+                <nav className="navbar navbar-default history">
+                    <div className="container-fluid">
+                        <HistoryBar />
+                    </div>
+                </nav>
+                <div className="container-fluid">
+                    <div className="row">
+                       {this.props.data.map(function(d){
+                           return <HistoryItem 
+                                key={d.Id} 
+                                id={d.Id}
+                                passed={d.Passed}
+                                failed={d.Failed}
+                                dateCompleted={d.DateCompleted}
+                                dateStarted={d.DateStarted}
+                                clickEventHandler = {clickHanlded(d.Id)}
+                           />
+                       })}
+                    </div>
+                </div> 
+            </div>
+        );
+    }
+})
+
+var Page = React.createClass({
+    getInitialState: function(){
+        var defaultUrl = "http://localhost:9111/tests/history";
+        var fakeData = fakeHistoryData;
+        return ({
+            url: defaultUrl,
+            data: fakeData,
+            content: <HistoryContent data={fakeData} clickEventHandler = {this.handleChildClick}/>,
+        });
+    },
+    componentDidMount: function(){
+        this.loadDataFromServer(); 
+    },
+    loadDataFromServer: function() {
+        $.ajax({
+            url: this.state.url,
+            dataType: 'json',
+            cache: false,
+            success: function(data) {
+                this.setState({
+                    data: data,
+                    content: <HistoryContent data={data} clickEventHandler = {this.handleChildClick}/>
+            });
+            }.bind(this),
+            error: function(xhr, status, err) {
+                console.error(this.props.url, status, err.toString());
+            }.bind(this)
+        });
+    },
+    handleChildClick: function (id) {
+       const _this = this;
+       return function(event){
+              var testData = _this.state.data[id].Tests;
+              _this.setState({
+                content: <ResultsPreviewCotent data={testData} />
+              });
+        }
+    },
+    render: function(){
+        return(
+            this.state.content
+        )
+    }
+});
+
 ReactDOM.render(
-    <PageContent url="http://localhost:9111/tests/fails"/>,
+    <Page/>,
     document.getElementById('root')
 )
-
-var $imageContainers = $("#leftImage, #rightImage");
-
-var sync = function(e){
-    var $other = $imageContainers.not(this).off('scroll'), other = $other.get(0);
-        
-    var verticalPercentage = this.scrollTop / (this.scrollHeight - this.offsetHeight);
-    var horizontalPercentage = this.scrollLeft / (this.scrollWidth - this.offsetWidth);
-    other.scrollLeft = horizontalPercentage * (other.scrollWidth - other.offsetWidth);
-    other.scrollTop = verticalPercentage * (other.scrollHeight - other.offsetHeight);
-    
-    setTimeout( function(){ $other.on('scroll', sync ); },10);
-}
-
-$imageContainers.on('scroll', sync);
